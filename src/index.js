e7c2b73d6f53eb84a2c29ab4b7f6d76c7e30efa8
Tsirov <anton121896@gmail.com>
2022-02-19 22:12:36 +0200

    Disconnect from vercel

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
-import { store, persistor } from './redux/store';
+import { store, persistor  } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
